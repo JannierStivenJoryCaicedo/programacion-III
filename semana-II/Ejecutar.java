05c91ac6be8885999b3cc5852d056e154b40a79f
JannierStivenJoryCaicedo <jsjory@estudiante.uniajc.edu.co>
2026-09-01 14:34:16 +0000

    llenar un arreglo de manera aleatoria con numeros del 1 al 100

--- semana-II/Ejecutar.java
+++ semana-II/Ejecutar.java
@@ -7,5 +7,30 @@ public class Ejecutar {
        for(int i = 0; i < a.length; i++){
         System.out.println("a["+ i + "]=" + a[i]);
        }
+
+       /*
+       int[]b = new int[10];
+
+       Random r = new Random();
+
+       for(int i = 0; i < b.length; i++){
+        b[i] = (r.nextInt()*10-1) + 1;
+       }
+    */
+
+       //forma 2 - aleatorio
+       
+       int[]b = new int[10];
+       int max = 100;
+       int min = 1;
+
+       for(int i = 0; i < b.length; i++){
+        b[i] = (int)(Math.random()*(max-min+1)) + min;
+       }
+
+       //mostrar el arreglo
+       for(int i = 0; i < b.length; i++){
+        System.out.println("b["+ i + "]=" + b[i]);
+       }
     }
 }
