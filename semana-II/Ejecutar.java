d254cc258ec076bc93673cfc413a8274873bd95d
JannierStivenJoryCaicedo <jsjory@estudiante.uniajc.edu.co>
2026-09-01 13:49:17 +0000

    Explicacion de arreglos

---
+++ semana-II/Ejecutar.java
@@ -0,0 +1,11 @@
+public class Ejecutar {
+    public static void main(String[] args) {
+       //forma 1 - crear arreglo
+       int[]a = {2, 8, 10, 6, 15, 20, 21, 1, 3, 12};
+
+       //recorrer arreglo
+       for(int i = 0; i < a.length; i++){
+        System.out.println("a["+ i + "]=" + a[i]);
+       }
+    }
+}
