3e2b191afa3ca76062f233b68742f69fcd3c048f
JannierStivenJoryCaicedo <jsjory@estudiante.uniajc.edu.co>
2026-09-08 14:07:00 +0000

    matrices

--- semana-III/MatricesA2.java
+++ semana-III/MatricesA2.java
@@ -3,10 +3,20 @@ public class MatricesA2 {
         //declaracion de la matriz
         int[][]m = {{5, 8, 6},{1, 3, 2},{4, 9, 7}};
 
-        for(int i = 0; i < m.length; i++){
-            for(int j = 0; j < m[0].length; j++){
+        for(int i = 0; i < m.length; i++){ //m.length siempre es para filas
+            for(int j = 0; j < m[0].length; j++){ //m[0].length siempre es para columnas
                 System.out.println("m["+i+"]["+j+"]="+m[i][j]);
             }           
-        }      
+        }
+        
+        //visualizar de mejor manera la matriz
+        String cad = "";
+        for(int i = 0; i < m.length; i++){ //m.length siempre es para filas
+            for(int j = 0; j < m[0].length; j++){ //m[0].length siempre es para columnas
+                cad += "|"+m[i][j];
+            }
+            cad += "|\n";
+        }
+        System.out.println(cad);
     }
 }
